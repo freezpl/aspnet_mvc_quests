05780134063001db0d44bc2674f52504824c393c
freezpl <freezpl@gmail.com>
2019-08-30 08:22:04 +0300

    init_commit

---
+++ WebApplication1/WebApplication1/Controllers/QuestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuestWebApp.Controllers
+{
+    public class QuestController : Controller
+    {
+        public IActionResult Quests()
+        {
+
+            return View();
+        }
+    }
+}
